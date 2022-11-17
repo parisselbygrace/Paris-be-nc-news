7dfbffa5587a2d2a5579b17d0e9d7bdb68a86e56
Paris Selby-Grace <paris.selby-grace1@nhs.net>
2022-11-17 16:09:14 +0000

    task 11 complete

--- db/db.js
+++ db/db.js
@@ -1,4 +1,16 @@
 const db = require('../db/connection.js');
+const format = require('pg-format')
+
+exports.checkExists = (table, column, value) => {
+    const queryStr = format('SELECT * FROM %I WHERE %I = %L;', table, column, value)
+    return db.query(queryStr).then((res) => {
+         if (res.rows.length === 0) {
+            return Promise.reject({status:404, msg: 'Id does not exisit'})
+        }
+        return res.rows[0]
+        
+    })
+}
 
 exports.checkArticleExists = (article_id) => {
     return db.query('SELECT * FROM articles WHERE article_id =$1', [article_id]).then((res) => {
@@ -13,8 +25,8 @@ exports.checkArticleExists = (article_id) => {
 exports.checkTopicExists = (topic) => {
   return db
     .query(`SELECT * FROM topics WHERE slug = $1`, [topic])
-    .then((result) => {
-      if (result.rows.length === 0) {
+    .then((res) => {
+      if (res.rows.length === 0) {
         return Promise.reject({ status: 404, msg: "Topic not found" })
       }
     });
